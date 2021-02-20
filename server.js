7139280a9576f8a34c9fcbee59b255f6d284d5c6
askador <fedosov0912@gmail.com>
2021-02-21 00:17:16 +0200

    first commit

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/html/TEST.html');
 });
 
+io.sockets.on('connection', function (socket) {
+  socket.on('browser_slider', function(val) { 
+    socket.emit('data', "slider move")
+  })
+})
+
 server.listen(port, function(){
   console.log('Server started')
 });
