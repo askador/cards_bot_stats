8751d290cc52030b95156b1c59430c01bdef1afb
askador <fedosov0912@gmail.com>
2021-02-21 00:43:47 +0200

    first commit

--- server.js
+++ server.js
@@ -32,60 +32,8 @@ app.get('/', function(req, res){
 
 io.sockets.on('connection', function (socket) {
   socket.on('browser_slider', function(val) {        
-    socket.emit('chart_data', "get_data(results)")
-       
-    // function get_data(results) {
-    //   var date = []
-    //   var count = []
-    //   var average = 0
-    //   var average_list = []
-
-    //   results.forEach(element => {
-    //       date.push(element['date'].toString().split(' ')[4])
-    //       count.push(element['count'])
-    //       average += element['count']
-    //     })
-
-    //   average /= date.length
-
-    //   for (let index = 0; index < date.length; index++) {
-    //       average_list.push(average)
-        
-    //   }
-
-    //   return {date, count, average_list}
-
-    // }
-
-    // clearInterval(chart_interval);
-
-    // if (val == 1) {
-    //   chart_interval = setInterval(function(){        
-    //     db.query("SELECT date, count FROM test_stats WHERE id >= (SELECT max(id) FROM test_stats) - 500", function (err, results) { 
-
-    //     socket.emit('chart_data', get_data(results))
-          
-    //     })}, 1000);
-
-    // }  
-    // else if (val == 2) {
-    //   chart_interval = setInterval(function(){        
-    //     db.query("SELECT date, count FROM test_stats WHERE id >= (SELECT max(id) FROM test_stats) - 100", function (err, results) { 
-
-    //       socket.emit('chart_data', get_data(results))
-            
-    //       })}, 1000);
-        
-    // }
-    // else if (val == 3) {
-    //   chart_interval = setInterval(function(){        
-    //     db.query("SELECT date, count FROM test_stats  WHERE id >= (SELECT max(id) FROM test_stats) - 10", function (err, results) { 
-
-    //       socket.emit('chart_data', get_data(results))
-            
-    //       })}, 1000);
-        
-    // }
+    console.log("DASTA")
+    // socket.emit('chart_data', "get_data(results)")
   });
 })
 
